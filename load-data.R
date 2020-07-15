378627f76b8de34cc2e971c112ee43217f85ca20
Vivek Katial <vivekkatial@gmail.com>
2020-07-15 12:41:05 +1200

    Update load-data.R

--- load-data.R
+++ load-data.R
@@ -8,6 +8,6 @@
 library(tidyverse)
 library(googlesheets4)
 
-gs4_auth(email = "vivekkatial@gmail.com")
+gs4_auth(email = "YOUR_EMAIL_HERE")
 d_movies <- read_sheet(as_sheets_id("1OtubhG6OTV0OyoDv4P_Hjl_okUzo5T0e9FZuCiTMVyw")) %>% 
     write_csv("movies.csv")
